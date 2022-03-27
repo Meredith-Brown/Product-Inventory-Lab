93451c7fc41a2f83a29ebbe69af7dca9257352fb
meredith <meredithbrown302@gmail.com>
2022-03-27 00:45:24 -0400

    Cookie getters and setters and tests

--- src/main/java/models/Cookie.java
+++ src/main/java/models/Cookie.java
@@ -8,5 +8,53 @@ public class Cookie {
     private int calories;
     private boolean containsNuts;
     private int quantity;
-    private int price;
+    private double price;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setContainsNuts(boolean containsNuts) {
+        this.containsNuts = containsNuts;
+    }
+
+    public boolean getContainsNuts() {
+        return containsNuts;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/test/java/models/CookieTest.java
+++ src/test/java/models/CookieTest.java
@@ -1,7 +1,81 @@
 package models;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CookieTest {
 
+    @Test
+    public void setNameTest() {
+        // given
+        String expected = "Chocolate Chip";
+        // when
+        Cookie testCookie = new Cookie();
+        testCookie.setName(expected);
+        // then
+        Assert.assertEquals(expected, testCookie.getName());
+    }
+
+    @Test
+    public void setIngredientsTest() {
+        // given
+        List<String> expected = new ArrayList<>();
+        expected.add("chocolate chips");
+        expected.add("flour");
+        expected.add("sugar");
+        // when
+        Cookie testCookie = new Cookie();
+        testCookie.setIngredients(expected);
+        // then
+        Assert.assertEquals(expected, testCookie.getIngredients());
+    }
+
+    @Test
+    public void setCaloriesTest() {
+        // given
+        int expected = 100;
+        // when
+        Cookie testCookie = new Cookie();
+        testCookie.setCalories(expected);
+        // then
+        Assert.assertEquals(expected, testCookie.getCalories());
+    }
+
+    @Test
+    public void setContainsNutsTest() {
+        // given
+        boolean expected = true;
+        // when
+        Cookie testCookie = new Cookie();
+        testCookie.setContainsNuts(expected);
+        // then
+        Assert.assertEquals(expected, testCookie.getContainsNuts());
+    }
+
+    @Test
+    public void setQuantityTest() {
+        // given
+        int expected = 50;
+        // when
+        Cookie testCookie = new Cookie();
+        testCookie.setQuantity(expected);
+        // then
+        Assert.assertEquals(expected, testCookie.getQuantity());
+    }
+
+    @Test
+    public void setPriceTest() {
+        // given
+        double expected = 1.50;
+        // when
+        Cookie testCookie = new Cookie();
+        testCookie.setPrice(expected);
+        // then
+        Assert.assertEquals(expected, testCookie.getPrice(), .001);
+    }
 }
