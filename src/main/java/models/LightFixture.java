1aab11ce33aa7310b79b231642c1a441da1aae46
meredith <meredithbrown302@gmail.com>
2022-03-27 00:16:00 -0400

    LightFixture properties

--- src/main/java/models/LightFixture.java
+++ src/main/java/models/LightFixture.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.util.List;
+
 public class LightFixture {
+    private String partNumber;
+    private String manufacturer;
+    private String type;
+    private String finish;
+    private int colorCCT;
+    private int lumenOutput;
+    private int quantity;
+    private int price;
 }
