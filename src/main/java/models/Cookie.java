907227c1b5e265e35f39a43c6e7f8630c8f25fd2
meredith <meredithbrown302@gmail.com>
2022-03-27 00:03:34 -0400

    added Cookie fields

--- src/main/java/models/Cookie.java
+++ src/main/java/models/Cookie.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.List;
+
 public class Cookie {
+    private String name;
+    private List<String> ingredients;
+    private int calories;
+    private boolean containsNuts;
+    private int quantity;
+    private int price;
 }
